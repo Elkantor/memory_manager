96b1334ef340e7cc802ce9753344f6cba0012182
Elkantor <vic03@live.fr>
2020-08-25 22:03:11 +0200

    Rework the memory_manager module

--- memory_manager.c
+++ memory_manager.c
@@ -1,65 +1,66 @@
-#include <stdlib.h>
+#include <stdlib.h> // for exit()
 
-struct internal_memory_manager
+/******************************************** CONFIG *******************************************************/
+// #define MEMORY_MANAGER_RELEASE 1
+#define MEMORY_MANAGER_BYTE_MAX 100000
+
+
+/******************************************** DEBUG/RELEASE MODES ******************************************/
+#ifdef MEMORY_MANAGER_RELEASE
+    // For Release
+    #define memory_manager_alloc(bytes)     memory_manager_alloc_release(bytes)
+    #define memory_manager_dealloc(pointer) memory_manager_dealloc_release((void*) pointer)
+#else
+    // For Debug
+    #define memory_manager_alloc(bytes)     memory_manager_alloc_debug(bytes, __LINE__, __FILE__)
+    #define memory_manager_dealloc(pointer) memory_manager_dealloc_debug((void*) pointer, __LINE__, __FILE__)
+#endif
+
+
+/******************************************** VARIABLES ****************************************************/
+static char internal_memory_manager_bytes_stack[MEMORY_MANAGER_BYTE_MAX];
+static int  internal_memory_manager_bytes_allocated = 0;
+
+
+/******************************************** PROCEDURES ***************************************************/
+void* memory_manager_alloc_release(int in_bytes)
 {
-    int     internal_memory_manager_int_array[1000];
-    float   internal_memory_manager_float_array[1000];
-    double  internal_memory_manager_double_array[1000];
-
-    int     internal_memory_manager_ints_allocated;
-
-    float   internal_memory_manager_floats_allocated;
-    double  internal_memory_manager_doubles_allocated;
-};
-
-struct internal_memory_manager* internal_memory_manager_handle;
-
-void memory_manager_init()
-{
-    internal_memory_manager_handle = malloc( (sizeof *internal_memory_manager_handle) );
-    internal_memory_manager_handle->internal_memory_manager_ints_allocated              = 0;
-    internal_memory_manager_handle->internal_memory_manager_floats_allocated            = 0;
-    internal_memory_manager_handle->internal_memory_manager_doubles_allocated           = 0;
-}
-
-void memory_manager_destroy()
-{
-    free(internal_memory_manager_handle);
-}
-
-#define memory_manager_int_alloc(n) memory_manager_int_alloc_internal(n, __LINE__, __FILE__)
-
-int* memory_manager_int_alloc_internal(int in_number, char* in_line, char* in_file)
-{
-    int* pointed_address = internal_memory_manager_handle->internal_memory_manager_int_array + internal_memory_manager_handle->internal_memory_manager_ints_allocated;
-    internal_memory_manager_handle->internal_memory_manager_ints_allocated += in_number;
-    printf("[MEMORY]: %d int allocated (%I64d bytes) | TOTAL INT ALLOCATED: %d\n", in_number, in_number * sizeof(int), internal_memory_manager_handle->internal_memory_manager_ints_allocated);
+    char* pointed_address = internal_memory_manager_bytes_stack + internal_memory_manager_bytes_allocated;
+    internal_memory_manager_bytes_allocated += in_bytes;
     return pointed_address;
 }
 
-void memory_manager_int_dealloc(int* out_value)
+void* memory_manager_alloc_debug(int in_bytes, int in_line, char* in_file)
 {
-    int* current_address_available = internal_memory_manager_handle->internal_memory_manager_int_array + internal_memory_manager_handle->internal_memory_manager_ints_allocated;
-    if (out_value >= current_address_available)
+    int max_bytes_available = sizeof(internal_memory_manager_bytes_stack)/sizeof(internal_memory_manager_bytes_stack[0]);
+    if (internal_memory_manager_bytes_allocated + in_bytes > max_bytes_available)
     {
-        printf("[MEMORY]: Try to deallocate memory that wasn't the last allocated\n");
-        free(internal_memory_manager_handle);
+        printf("\n[MEMORY]: line %d in %s| Can't allocate more than %d bytes in this program\n", in_line, in_file, max_bytes_available);
         exit(1);
     }
 
-
-    int number_deallocated = current_address_available - out_value;
-    internal_memory_manager_handle->internal_memory_manager_ints_allocated -= number_deallocated;
-    printf("[MEMORY]: %d int freed (%I64d bytes) | TOTAL INT ALLOCATED: %d\n", number_deallocated, number_deallocated * sizeof(int), internal_memory_manager_handle->internal_memory_manager_ints_allocated);
-}
-
-int memory_manager_int_allocated_get()
-{
-    return internal_memory_manager_handle->internal_memory_manager_ints_allocated;
-}
-
-int* memory_manager_int_allocated_last()
-{
-    int* pointed_address = internal_memory_manager_handle->internal_memory_manager_int_array + internal_memory_manager_handle->internal_memory_manager_ints_allocated;
+    char* pointed_address = internal_memory_manager_bytes_stack + internal_memory_manager_bytes_allocated;
+    internal_memory_manager_bytes_allocated += in_bytes;
+    printf("[MEMORY]: line %d: in %s \t\t| %d bytes allocated\t\t| TOTAL BYTES ALLOCATED: %d\n", in_line, in_file, in_bytes, internal_memory_manager_bytes_allocated);
     return pointed_address;
+}
+
+void memory_manager_dealloc_release(void* out_value)
+{
+    void* current_address_available = internal_memory_manager_bytes_stack + internal_memory_manager_bytes_allocated;
+    int number_deallocated = current_address_available - out_value;
+    internal_memory_manager_bytes_allocated -= number_deallocated;
+}
+
+void memory_manager_dealloc_debug(void* out_value, int in_line, char* in_file)
+{
+    void* current_address_available = internal_memory_manager_bytes_stack + internal_memory_manager_bytes_allocated;
+    if (out_value >= current_address_available)
+    {
+        printf("\n[MEMORY]: Error line %d in %s | Try to deallocate memory that wasn't the last allocated\n", in_line, in_file);
+        exit(1);
+    }
+    int number_deallocated = current_address_available - out_value;
+    internal_memory_manager_bytes_allocated -= number_deallocated;
+    printf("[MEMORY]: line %d: in %s \t\t| %d bytes freed\t\t| TOTAL BYTES ALLOCATED: %d\n", in_line, in_file, number_deallocated, internal_memory_manager_bytes_allocated);
 }
