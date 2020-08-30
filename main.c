3a09d18f759db1dddde8c3c2abdebaeb07cb05b2
Elkantor <vic03@live.fr>
2020-08-30 11:20:38 +0200

    Update main.c examples

--- main.c
+++ main.c
@@ -4,10 +4,15 @@
 int main()
 {
     // Here is a quick example how to use the memory_manager module
+    // int* array          = memory_manager_alloc(3 * sizeof(int));
+    // int* array2         = memory_manager_alloc(100 * sizeof(int));
+    // float* array_float  = memory_manager_alloc(5 * sizeof(float));
+    memory_manager_alloc(int, array, 10);
+    memory_manager_alloc(int, array2, 100);
+    memory_manager_alloc(float, array_float, 5);
 
-    int* array          = memory_manager_alloc(3 * sizeof(int));
-    int* array2         = memory_manager_alloc(100 * sizeof(int));
-    float* array_float  = memory_manager_alloc(5 * sizeof(float));
+    for (int i = 0; i < 5; ++i)
+        array_float[i] = 2.f;
 
     memory_manager_dealloc(array_float);
     memory_manager_dealloc(array2);
