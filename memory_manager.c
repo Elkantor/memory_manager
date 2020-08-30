bf75a9f578193da8688920f039c2f0e262ac48df
Elkantor <vic03@live.fr>
2020-08-30 16:30:09 +0200

    Fix default memory size max allocated to 64Mo

--- memory_manager.c
+++ memory_manager.c
@@ -2,18 +2,26 @@
 
 /******************************************** CONFIG *******************************************************/
 // #define MEMORY_MANAGER_RELEASE 1
-#define MEMORY_MANAGER_BYTE_MAX 100000
+#define MEMORY_MANAGER_BYTE_MAX 64000000 // using 64Mo
 
+#define defer _d0(__COUNTER__)
+#define _d0(X) _d1(X)
+#define _d1(X) _d2(s##X,f##X)
+#define _d2(S,F) auto void F(void*);int S __attribute__((cleanup(F)));void F(void*_)
 
 /******************************************** DEBUG/RELEASE MODES ******************************************/
 #ifdef MEMORY_MANAGER_RELEASE
     // For Release
-    #define memory_manager_alloc(variable_type, variable_name, number)  variable_type* variable_name = memory_manager_alloc_release(sizeof(variable_type) * number)
     #define memory_manager_dealloc(pointer)                             memory_manager_dealloc_release((void*) pointer)
+    #define memory_manager_alloc(variable_type, variable_name, number)\
+        variable_type* variable_name = memory_manager_alloc_release(sizeof(variable_type) * number);\
+        defer { memory_manager_dealloc(variable_name); }
 #else
     // For Debug
-    #define memory_manager_alloc(variable_type, variable_name, number)  variable_type* variable_name = memory_manager_alloc_debug(sizeof(variable_type) * number, __LINE__, __FILE__, #variable_name)
     #define memory_manager_dealloc(pointer)                             memory_manager_dealloc_debug((void*) pointer, __LINE__, __FILE__, #pointer)
+    #define memory_manager_alloc(variable_type, variable_name, number)\
+        variable_type* variable_name = memory_manager_alloc_debug(sizeof(variable_type) * number, __LINE__, __FILE__, #variable_name);\
+        defer { memory_manager_dealloc(variable_name); }
 #endif
 
 /******************************************** VARIABLES ****************************************************/
@@ -40,7 +48,7 @@ void* memory_manager_alloc_debug(int in_bytes, int in_line, char* in_file, char*
 
     char* pointed_address = internal_memory_manager_bytes_stack + internal_memory_manager_bytes_allocated;
     internal_memory_manager_bytes_allocated += in_bytes;
-    printf("[MEMORY]: %s \t\t\tline %d: in %s \t\t| %d bytes allocated\t\t| TOTAL BYTES ALLOCATED: %d\n", in_variable_name, in_line, in_file, in_bytes, internal_memory_manager_bytes_allocated);
+    printf("[MEMORY]: %s \t\t\tline %d: in %s \t\t\t| %12d bytes allocated\t| TOTAL BYTES ALLOCATED: %d\n", in_variable_name, in_line, in_file, in_bytes, internal_memory_manager_bytes_allocated);
     return pointed_address;
 }
 
@@ -56,10 +64,10 @@ void memory_manager_dealloc_debug(void* out_value, int in_line, char* in_file, c
     void* current_address_available = internal_memory_manager_bytes_stack + internal_memory_manager_bytes_allocated;
     if (out_value >= current_address_available)
     {
-        printf("\n[MEMORY]: Error line %d in %s | Try to deallocate memory that wasn't the last allocated\n", in_line, in_file);
+        printf("\n[MEMORY]: Error line %d in %s | Try to deallocate memory that wasn't the last allocated (%s)\n", in_line, in_file, in_variable_name);
         exit(1);
     }
     int number_deallocated = current_address_available - out_value;
     internal_memory_manager_bytes_allocated -= number_deallocated;
-    printf("[MEMORY]: %s \t\t\tline %d: in %s \t\t| %d bytes freed\t\t| TOTAL BYTES ALLOCATED: %d\n", in_variable_name, in_line, in_file, number_deallocated, internal_memory_manager_bytes_allocated);
+    printf("[MEMORY]: %s \t\t\tline %d: in %s \t\t\t| %12d bytes freed\t| TOTAL BYTES ALLOCATED: %d\n", in_variable_name, in_line, in_file, number_deallocated, internal_memory_manager_bytes_allocated);
 }
