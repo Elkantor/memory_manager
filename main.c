95cb08432102206d15cd917858b4653b157d1ce0
Elkantor <vic03@live.fr>
2020-08-30 16:31:00 +0200

    Show automaticly free allocated value at the end of the scope using defer instruction, only available on gcc and clang

--- main.c
+++ main.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include "memory_manager.c"
 
+
 int main()
 {
     // Here is a quick example how to use the memory_manager module
-    memory_manager_alloc(int, array, 10);
-    memory_manager_alloc(int, array2, 100);
-    memory_manager_alloc(float, array_float, 5);
+    {
+        memory_manager_alloc(int, array, 2000000);
+        for (int i = 0; i < 2000000; ++i)
+            array[i] = 10;
 
-    for (int i = 0; i < 5; ++i)
-        array_float[i] = 2.f;
-
-    memory_manager_dealloc(array_float);
-    memory_manager_dealloc(array2);
-    memory_manager_dealloc(array);
+        memory_manager_alloc(float, array2, 12);
+        for (int i = 0; i < 12; ++i)
+            array2[i] = 2.f;
+    }
 
     return 0;
 }
